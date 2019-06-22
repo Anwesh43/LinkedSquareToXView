74349a28e82852d18e6817b82011f00c1516a6cb
Anwesh43 <anweshthecool0@gmail.com>
2019-06-23 01:42:21 +0530

    adding SquareToX class

--- squaretoxview/src/main/java/com/anwesh/uiprojects/squaretoxview/SquareToXView.kt
+++ squaretoxview/src/main/java/com/anwesh/uiprojects/squaretoxview/SquareToXView.kt
@@ -166,7 +166,31 @@ class SquareToXView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class SquareToX(var i : Int) {
+
+        private val root : STXNode = STXNode(0)
+        private var curr : STXNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
