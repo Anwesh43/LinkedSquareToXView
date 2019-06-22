9b27571fc3a3e29e8c0dbd079639b64460711543
Anwesh43 <anweshthecool0@gmail.com>
2019-06-23 01:34:29 +0530

    adding SquareToXView class

--- squaretoxview/src/main/java/com/anwesh/uiprojects/squaretoxview/SquareToXView.kt
+++ squaretoxview/src/main/java/com/anwesh/uiprojects/squaretoxview/SquareToXView.kt
@@ -60,3 +60,21 @@ fun Canvas.drawSTXNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class SquareToXView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
