313f7647b8a2d38bcab3ea509c82be916876df6f
Anwesh43 <anweshthecool0@gmail.com>
2019-06-23 01:24:38 +0530

    adding libraries and constants

--- squaretoxview/src/main/java/com/anwesh/uiprojects/squaretoxview/SquareToXView.kt
+++ squaretoxview/src/main/java/com/anwesh/uiprojects/squaretoxview/SquareToXView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.squaretoxview
  * Created by anweshmishra on 23/06/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 45f
